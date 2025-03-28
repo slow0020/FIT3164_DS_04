1ed27f5356bf30c23a2b173cc3bde1ead80ae646
ZiJun-0423 <weezijun@gmail.com>
2025-03-28 21:42:13 +1100

    Created file for initial import of historical data to db

--- backend/database/create_table.sql
+++ backend/database/create_table.sql
@@ -5,37 +5,45 @@ DROP TABLE IF EXISTS elo_rating;
 
 
 CREATE TABLE team (
-    team_id         INT NOT NULL,
-    team_name       VARCHAR (100) NOT NULL,
-    home_venue      VARCHAR (100)
+    team_id             INT NOT NULL,
+    team_name           VARCHAR (100) NOT NULL,
+    team_home_venue     VARCHAR (100)
 );
 
 ALTER TABLE team
     ADD (
         CONSTRAINT team_pk PRIMARY KEY (team_id)
     );
-
+ALTER TABLE team
+    MODIFY COLUMN team_id INT NOT NULL AUTO_INCREMENT;
 CREATE TABLE game (
     game_id         INT NOT NULL,
-    year            DATE NOT NULL,
+    year            INT NOT NULL,
     round_no        VARCHAR (3) NOT NULL,
-    venue           VARCHAR (100)   
+    venue           VARCHAR (100),
+    team1_id        INT NOT NULL,
+    team2_id        INT NOT NULL
 );
 
 ALTER TABLE game
 ADD(
-    CONSTRAINT game_pk PRIMARY KEY (game_id)
+    CONSTRAINT game_pk PRIMARY KEY (game_id),
+    CONSTRAINT team1_id_fk FOREIGN KEY (team1_id) REFERENCES team(team_id),
+    CONSTRAINT team2_id_fk FOREIGN KEY (team2_id) REFERENCES team(team_id)
 );
+
+ALTER TABLE game
+    MODIFY COLUMN game_id INT NOT NULL AUTO_INCREMENT;
 
 CREATE TABLE game_team (
     game_team_id            INT NOT NULL,
     game_id                 INT NOT NULL,
     team_id                 INT NOT NULL,
-    is_home_team            TINYINT NOT NULL,
+    is_home_team            TINYINT,
     is_winner               TINYINT NOT NULL,
-    score                   INT NOT NULL,
+    score                   INT,
     goals                   INT NOT NULL,
-    behinds                 INT NOT NULL,
+    behinds                 INT,
     disposals               INT,
     kicks                   INT,
     marks                   INT,
@@ -63,6 +71,8 @@ ALTER TABLE game_team
         CONSTRAINT game_team_game_fk FOREIGN KEY (game_id) REFERENCES game(game_id),
         CONSTRAINT game_team_team_fk FOREIGN KEY (team_id) REFERENCES team(team_id)
     );
+ALTER TABLE game_team
+    MODIFY COLUMN game_team_id INT NOT NULL AUTO_INCREMENT; 
 
 CREATE TABLE elo_rating (
     elo_rating_id       INT NOT NULL,
@@ -79,3 +89,6 @@ ALTER TABLE elo_rating
         CONSTRAINT elo_rating_team_fk FOREIGN KEY (team_id) REFERENCES team(team_id),
         CONSTRAINT elo_rating_game_fk FOREIGN KEY (game_id) REFERENCES game(game_id)
     );
+    
+ALTER TABLE elo_rating 
+	MODIFY COLUMN elo_rating_id INT NOT NULL AUTO_INCREMENT;
